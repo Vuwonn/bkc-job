76bfce5eeb60d0bad8986c6ef8b00c2ff9d9a3b9
Vuwonn <bwnchy245@gmail.com>
2024-12-07 22:47:10 +0545

    comple

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT="bkc-job-h1d4-5evihr1t7-bhuwan-chaudharys-projects.vercel.app/api/v1/user";
+export const JOB_API_END_POINT="bkc-job-h1d4-5evihr1t7-bhuwan-chaudharys-projects.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT="bkc-job-h1d4-5evihr1t7-bhuwan-chaudharys-projects.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT="bkc-job-h1d4-5evihr1t7-bhuwan-chaudharys-projects.vercel.app/api/v1/company";
