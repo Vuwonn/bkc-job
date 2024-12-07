006bcd57da02a512afa5dc842c07ddf0526ccb3e
Vuwonn <bwnchy245@gmail.com>
2024-12-08 00:44:10 +0545

    commerse

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
     origin:'https://bkc-job-21k8.vercel.app',
+    methods:['GET,POST,PUT,DELETE'],
     credentials:true
 }
 
