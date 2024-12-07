8a93f84fff06d92174f3da267ad4ca6e2d09cfd8
Vuwonn <bwnchy245@gmail.com>
2024-12-08 00:37:03 +0545

    cors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://bkc-job-21k8.vercel.app/',
+    origin:'https://bkc-job-21k8.vercel.app',
     credentials:true
 }
 
